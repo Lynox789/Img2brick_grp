0789f55af88876a38594510e77be161793f8b964
sebastien <dumur.seb@gmail.com>
2026-04-10 12:54:53 +0200

    Correction of a bug with database path

--- api/mobile/install.php
+++ api/mobile/install.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // Connexion à la base de données (adapter selon votre configuration)
-require_once __DIR__ . '/../../includes/db_connect.php';
+require_once __DIR__ . '/../../connexion/Database.php';
 
 try {
     $data = json_decode(file_get_contents('php://input'), true);
